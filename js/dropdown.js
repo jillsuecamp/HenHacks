0cb9d16e5f10848a7b22ba0f1a77c5811d6efcd5
DylanMinchhoff <dylanminchhoff@gmail.com>
2023-11-07 18:56:56 -0500

    Triangles added for dropdowns on faqs rotate animation needed for when dropdown is selected
    faqs are now dropdowns
    faqs are contained in the _faq.yml file in the _data section
    generic dropdown classes created for dropdowns

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,15 +1,21 @@
 /**
  * 
- * @param {any} scrElement - the scrElement is the button the triggered the event
+ * @param {Event} e - the event from the element that triggered the dropdown
  */
-function toggleDropdown(srcElement) {
-    if(srcElement.children) {
-        
+function triggerDropdown(e) {
+    toggleDropdown(e.target)
+}
+/**
+ * 
+ * @param {any} target - the target is the button the triggered the event
+ */
+function toggleDropdown(target) {
+    if(target.children) {
         // looping through the srcElement's childern
         // when a child is found with the class of "dropdown-content" it's 
         // visability is toggled
-        const subNodes = srcElement.children;
-        for (let i = 0; i < srcElement.children.length; i++) {
+        const subNodes = target.children;
+        for (let i = 0; i < target.children.length; i++) {
             if(subNodes[i].classList.contains("dropdown-content")) {
                 subNodes[i].classList.toggle("show-dropdown");
             }
